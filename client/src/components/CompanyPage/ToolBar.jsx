3cba4c4db7484dee9774a3ba3fd7c0cb6b2a3b3f
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-09-16 14:15:51 -0500

    baisc tool bar buttons made, non functional

---
+++ client/src/components/CompanyPage/ToolBar.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function Toolbar() {
+
+  return (
+    <div className="flex flex-row space-x-4">
+
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Write Review
+    </button>
+
+    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+      Share
+    </button>
+
+    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+      Wish I Had Bean There
+    </button>
+
+    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      Bean There
+    </button>
+
+  </div>
+  );
+
+}
--- client/src/pages/Company.jsx
+++ client/src/pages/Company.jsx
@@ -1,6 +1,6 @@
 import PhotoCarousel from '../components/CompanyPage/PhotoCarousel'
 import Reviews from '../components/CompanyPage/Reviews';
-
+import Toolbar from '../components/CompanyPage/ToolBar';
 export default function Company() {
   return (
     <div className='h-screen mt-10'>
@@ -12,8 +12,8 @@ export default function Company() {
     </div>
     <div className='flex justify-center pt-32 '>
     <div className='grid  grid-cols-3 grid-rows-2 gap-4 w-3/5'>
-      <div id="tool-bar" className='bg-green-700 col-span-2 '>
-      <p>Tool Bar</p>
+      <div id="tool-bar" className='bg-green-700 col-span-2 flex justify-evenly'>
+      <Toolbar/>
       </div>
       <div id="info-cards" className='bg-green-700'>
       <p>info cards</p>
