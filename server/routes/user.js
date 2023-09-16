3dc6e3ac9a62b8b7378ed2de99bf77886ffcda80
destocot <khurramcali@gmail.com>
2023-09-16 16:21:03 -0400

    chat routes / chat controllers / mock data updated

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var router = express.Router();
 var { login, register } = require("../controllers/usersController");
+// all these endpoints start with /user*
 router.post("/login", login);
 router.post("/register", register);
 module.exports = router;
