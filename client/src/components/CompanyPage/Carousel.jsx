b6ceb367aa63ee62bd460b24d09309cbb792c46a
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-09-17 14:35:26 -0500

    changes css for carousel

--- client/src/components/CompanyPage/Carousel.jsx
+++ client/src/components/CompanyPage/Carousel.jsx
@@ -5,27 +5,28 @@ import Slider from "react-slick";
 
 export default function SimpleSlider( {photos} ) {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     adaptiveHeight: true,
-    variableWidth: true
+
   };
   return (
+    <div className="max-w-full mx-auto">
+
     <Slider {...settings}>
       {photos.map((image, index) => (
-              <div key={index} className="">
-                <div className="">
-                  <img
-                    src={image}
-                    className="rounded-box opacity-100 w-64 h-64 object-cover"
-                  />
-                </div>
-
-              </div>
-            ))}
+        <div key={index} className=" px-2 py-2 ">
+            <img
+              key={index}
+              src={image}
+              className="rounded-box opacity-100 w-full max-h-96 object-cover"
+            />
+        </div>
+          ))}
     </Slider>
+    </div>
   );
 }
