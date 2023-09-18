c53938b15444a4e2c69ebd6b695a53bad6a67d67
Vincent <vincentcolano@gmail.com>
2023-09-18 15:38:39 -0500

    messenger now uses api data

--- client/src/pages/Messenger.jsx
+++ client/src/pages/Messenger.jsx
@@ -1,70 +1,22 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import ChatGroups from '../components/Messenger/ChatGroups.jsx';
+import Messages from '../components/Messenger/Messages.jsx';
 
-const USER_ID = 1;
-const url = 'http://localhost:5000/messenger/'
+var USER_ID = 1;
+
 export default function Messenger() {
-  const testChat = (key) => {
-    var coinFlip = Math.floor(Math.random() * 2);
-    console.log(coinFlip);
-    return (
-      <div key={key} className={`chat chat-${coinFlip ? 'start' : 'end'}`}>
-        <div className="chat-image avatar">
-          <div className="w-10">
-            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div className="chat-header">
-          Obi-Wan Kenobi
-          <time className="text-xs opacity-50">12:45</time>
-        </div>
-        <div className="chat-bubble">Not leave it in  DarknessDarkness   DarknessDarkness</div>
-      </div>
-    );
-  }
-  const testGroup = (key) => {
-    return (
-      <div key={key}>
-        {key !== 0 ? (
-          <div className="divider m-3"></div>
-        ) : (<div></div>)}
-        <div className="w-full flex items-start justify-center flex-col bg-[#212121] cursor-pointer h-[113px] rounded-[11px] border-[1.5px] border-grey-500">
-          <p className="font-bold">Chat Name</p>
-          <p>
-            Member1, Member2, Member3
-          </p>
-          <p>
-            Member2: Last Message
-          </p>
-        </div>
-      </div>
-    )
-  }
+  const [roomId, setRoomId] = useState(1);
+  const loading = USER_ID === undefined;
 
 
-  useEffect(() => {
-    axios.get(url + 'rooms/user/1').then(result => {
-      console.log(result.data);
-    }).catch(err => {
-      console.log(err);
-    })
-  }, []);
-
-  return (
+  return loading ? (<>Loading chats...</>) : (
     <div className="flex w-full  h-screen">
       <div className="w-4/12 h-full overflow-auto max-w-[250px] min-w-[240px] bg-[black]">
-        <div className="grid pt-5 grid-cols-1 justify-items-center">
-          {['', '', '', ''].map((e, i) => (
-            testGroup(i)
-          ))}
-        </div>
+        <ChatGroups id={USER_ID} setRoom={setRoomId}/>
       </div>
       <div className="w-full h-full flex flex-items-end justify-end flex-col">
-        <div className="flex w-full overflow-auto justify-start flex-col-reverse flex-items-end">
-          {['', '', '', ''].map((e, i) => (
-            testChat(i)
-          ))}
-        </div>
+        <Messages userId={USER_ID} roomId={roomId} />
         <div className="flex w-full h-[90px] bg-[black] items-center p-3">
           <textarea className="textarea resize-none w-[85%] h-full" placeholder="Bio"></textarea>
           <button className="btn btn-primary h-full ml-3 w-[75px]">send</button>
