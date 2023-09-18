0921f4593741c943b61cd58c935c2938755b9584
Tonegawa94 <victor1394@gmail.com>
2023-09-18 18:36:54 -0500

    added reviews data and rating section

--- client/src/components/CompanyPage/CompanyReviews.jsx
+++ client/src/components/CompanyPage/CompanyReviews.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import BeanRating from "./BeanRating";
+import moment from "moment"
 
 const CompanyReviews = (prop) => {
+  const date = prop.reviewData.updated_at
  return (
   <div className=" my-4 py-4">
     <div className="flex text-left items-center">
-      <img src="https://cdn.discordapp.com/attachments/1140681171617984683/1152697648093069342/image.png" className="rounded-e-full w-auto h-20"></img>
+      <img src="https://cdn.discordapp.com/attachments/1140681171617984683/1152697648093069342/image.png" className="rounded-full w-20 h-20 object-cover"></img>
       <div className="flex-col end pl-3 ">
       <h3>User McName</h3>
-      <p>★★★★★ 01/02/2023</p>
+      <p className="pt-1">{moment(date).format("MMM Do YYYY")}</p>
       </div>
     </div>
+    <div>
+      <div className="w-fit pt-4 pb-3">
+      <BeanRating rating={prop.reviewData.rating} />
+      </div>
     <div className="py-4">
-      <p>{prop.text}</p>
+      <p>{prop.reviewData.body}</p>
+    </div>
     </div>
   </div>
  )
--- client/src/components/CompanyPage/OverallRating.jsx
+++ client/src/components/CompanyPage/OverallRating.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import CompanyRatings from "./Progress";
 
-const OverallRating = () => {
+const OverallRating = (prop) => {
 return (
   <div className="pt-3 mt-4 flex justify-between ">
     <div className="self-center">
@@ -9,11 +10,7 @@ return (
       <p className="text-xs">420 reviews</p>
     </div>
     <div className="p-4">
-      <h3> 5 star   ⬛  ⬛  ⬛  ⬛  ⬜  </h3>
-      <h3> 4 star   ⬛  ⬛  ⬛  ⬜  ⬜  </h3>
-      <h3> 3 star   ⬛  ⬛  ⬜  ⬜  ⬜  </h3>
-      <h3> 2 star   ⬛  ⬜  ⬜  ⬜  ⬜  </h3>
-      <h3> 1 star   ⬛  ⬜  ⬜  ⬜  ⬜  </h3>
+      <CompanyRatings rating={prop.ratings}/>
     </div>
   </div>
 )
