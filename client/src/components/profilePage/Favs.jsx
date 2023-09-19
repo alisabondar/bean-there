cd2866e45034116d4958deaa5be828d3f73b90a3
destocot <khurramcali@gmail.com>
2023-09-19 15:38:31 -0400

    GET DATA

--- client/src/components/profilePage/Favs.jsx
+++ client/src/components/profilePage/Favs.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-function Favs() {
+function Favs({ wishlist }) {
   return (
-    <div>Favs</div>
-  )
+    <div className="w-full lg:w-9/12 px-4 mx-auto">
+      <h1>My Wishlist</h1>
+      {wishlist.length > 0 ? (
+        wishlist.map((item) => {
+          return (
+            <div key={item.id} className="border bg-accent/[0.2] p-2 m-2">
+              <p>{item.location_name}</p>
+            </div>
+          );
+        })
+      ) : (
+        <span>has no wishlist</span>
+      )}
+    </div>
+  );
 }
 
-export default Favs
+export default Favs;
