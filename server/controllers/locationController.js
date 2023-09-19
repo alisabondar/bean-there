8dff1a68e639d63808abf18dd8dc38041089c821
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-09-19 11:12:53 -0400

    implemented an api call in locationController

--- server/controllers/locationController.js
+++ server/controllers/locationController.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,14 +31,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
+const axios_1 = __importDefault(require("axios"));
+dotenv.config();
 var getLocations = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const query = req.params.query;
     console.log(query);
-    /**
-     * HERE YOU WOULD TAKE THE QUERY AND USE THE GOOGLE API
-     * OR YELP API AND SEND BACK THE DATA YOU WANT
-     */
-    res.status(200).send({ mssg: "reached getLocations controller" });
+    // optimize -> get someones loc for lat and long params
+    axios_1.default.get(`${process.env.GOOGLEAPI_URL}
+    ?keyword=coffee
+    &location=41.881832,-87.623177
+    &radius=5000
+    &key=${process.env.GOOGLEAPI_KEY}`)
+        .then(result => {
+        console.log(result);
+    })
+        .catch(err => {
+        console.error('Cannot fetch nearby locations', err);
+    });
+    // res.status(200).send({ mssg: "reached getLocations controller" });
 });
 module.exports = { getLocations };
--- server/controllers/locationController.ts
+++ server/controllers/locationController.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import axios from 'axios';
+
+dotenv.config();
 
 var getLocations = async (req: Request, res: Response) => {
   const query = req.params.query;
   console.log(query);
 
-  /**
-   * HERE YOU WOULD TAKE THE QUERY AND USE THE GOOGLE API
-   * OR YELP API AND SEND BACK THE DATA YOU WANT
-   */
+  // optimize -> get someones loc for lat and long params
+  axios.get(`${process.env.GOOGLEAPI_URL}
+    ?keyword=coffee
+    &location=41.881832,-87.623177
+    &radius=5000
+    &key=${process.env.GOOGLEAPI_KEY}`)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(err => {
+      console.error('Cannot fetch nearby locations', err)
+    })
 
-  res.status(200).send({ mssg: "reached getLocations controller" });
+  // res.status(200).send({ mssg: "reached getLocations controller" });
 };
 
 module.exports = { getLocations };
