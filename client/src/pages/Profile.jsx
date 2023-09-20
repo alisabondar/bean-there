aae32504c8b6d59deb28d891319409ace241864f
Heith Mahfoudhi <heith.mhd@gmail.com>
2023-09-20 15:54:57 -0400

    nav bar

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import axios from "axios";
 import Bio from "../components/profilePage/Bio.jsx";
 import Calendar from "../components/profilePage/Calendar.jsx";
 import Favs from "../components/profilePage/Favs.jsx";
 import Friends from "../components/profilePage/Friends.jsx";
-import Posts from "../components/profilePage/Posts.jsx";
+
 import {
-  BsFillPlusCircleFill,
   BsFillStarFill,
   BsFillCalendarCheckFill,
   BsFillChatDotsFill,
@@ -95,8 +94,6 @@ function Profile() {
         return <Favs wishlist={wishlist} />;
       case "friends":
         return <Friends friends={friends} />;
-      case "posts":
-        return <Posts />;
       case "bio":
         return <Bio about={profile.about} reviews={reviews} />;
       default:
@@ -168,7 +165,7 @@ function Profile() {
                   <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
                     <div
                       className="relative hover:scale-110 transition duration-300 ease-in-out cursor-pointer"
-                      onClick={() => handleButtonClick("posts")}
+                      onClick={() => handleButtonClick("bio")}
                     >
                       <img
                         alt="..."
@@ -186,7 +183,7 @@ function Profile() {
                       {/* Button: Add Post */}
                       <button
                         style={{ backgroundColor: "#A98E77" }}
-                        onClick={() => handleButtonClick("posts")}
+                        onClick={() => handleButtonClick("bio")}
                         className="uppercase text-[#3C2A21] font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 hover:scale-110 transition duration-300 ease-in-out"
                         type="button"
                       >
