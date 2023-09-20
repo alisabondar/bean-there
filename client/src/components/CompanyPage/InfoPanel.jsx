87099d75f7c812f2857393c27de10de623fe7457
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-09-20 15:23:58 -0500

    changed how website is displayed to user

--- client/src/components/CompanyPage/InfoPanel.jsx
+++ client/src/components/CompanyPage/InfoPanel.jsx
@@ -14,7 +14,7 @@ export default function InfoPanel( {business} ) {
     <div className="mx-auto p-5 mt-2 bg-neutral rounded-box border-accent border-[1rem]">
       <div className="border-b pb-4 mb-4">
         <h2 className="text-2xl font-bold mb-4">Business Details</h2>
-        <a href={business.website} className="hover:underline mb-4 block">Visit Coffee Shop Website</a>
+        <a href={business.website} className="hover:underline mb-4 block">{business.website}</a>
         <hr className="my-4" />
         <p className="mb-4">{stripHTMLTags(business.adr_address)}</p>
         <hr className="my-4" />
