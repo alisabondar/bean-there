0d93011121e108a73acbd511fbfa337edb32c2c0
destocot <khurramcali@gmail.com>
2023-09-22 09:48:12 -0400

    simple not found page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="w-screen h-screen bg-primary flex justify-center items-center">
+      <div className="text-5xl space-y-5 text-center">
+        <div>Woops! You Shouldn&apos;t Be Here</div>
+        <div>
+          Click{" "}
+          <Link to="/" className="underline inline-block hover:text-neutral">
+            Here
+          </Link>{" "}
+          To Bean There
+        </div>
+      </div>
+    </div>
+  );
 }
